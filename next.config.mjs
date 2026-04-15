d928e518f5305bbd04b533fb47148f64ead31ae3
icang ramdani <masanies748@gmail.com>
2026-04-15 19:42:20 +0700

    Fix Next.js build: exclude Firebase functions from build process

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // Exclude functions directory from Next.js build
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin', 'firebase-functions'],
+  },
+  // Ignore functions directory during build
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'firebase-admin': 'firebase-admin',
+        'firebase-functions': 'firebase-functions',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
